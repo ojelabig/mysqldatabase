69a55cdedf222266b278fbb2e852494039f632f2
ojelabig <98741453+ojelabig@users.noreply.github.com>
2022-02-21 13:05:18 +0000

    Delete Ojelabigrace.sql

--- Ojelabigrace.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM `Ojelabi`.`Student Details`;
-INSERT INTO `Ojelabi`.`Student Details` (`Student_ID`,`First_Name`,`Last_Name`,`DOB`,`Email_Address`,`Phone_No`,`Gender`) VALUES (@fn,@ln,@dob,@ea,@pn,@gd)
